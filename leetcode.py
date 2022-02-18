32a8a84f8e2741a220092d357f6458ceec06a4de
Joey Peterson <joey.peterson15@gmail.com>
2022-02-18 14:59:30 -0800

    structy

--- leetcode.py
+++ leetcode.py
@@ -695,6 +695,24 @@
 
 
 
+# strcuty ==> tree includes
+# Write a function, tree_includes, that takes in the root of a binary tree and a target value.
+# The function should return a boolean indicating whether or not the value is contained in the tree.
+
+
+
+# def tree_includes(root, target):
+#   if not root:
+#     return False
+#   if root.val == target:
+#     return True
+
+#   return tree_includes(root.left, target) or tree_includes(root.right, target)
+
+
+
+
+
 
 # Structy ==> tree min value
 # Write a function, tree_min_value, that takes in the root of a binary tree that contains number values.
