38822dfa4605c5dacb8b532c74614c4c797efc0e
Joey Peterson <joey.peterson15@gmail.com>
2022-02-18 15:07:11 -0800

    asdasf

--- leetcode.py
+++ leetcode.py
@@ -692,6 +692,32 @@
 
 
 
+# breadth first values
+# Write a function, breadth_first_values, that takes in the root of a binary tree.
+# The function should return a list containing all values of the tree in breadth-first order.
+
+# def breadth_first_values(root):
+#   pass # todo
+#   import collections
+#   res = []
+
+#   if not root:
+#     return res
+
+#   queue = []
+#   queue.append(root)
+
+
+
+#   while queue:
+#     node = queue.pop(0)
+#     res.append(node.val)
+#     if node.left:
+#       queue.append((node.left))
+#     if node.right:
+#       queue.append((node.right))
+
+#   return res
 
 
 
