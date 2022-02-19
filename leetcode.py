ba9619c93a2b60539ea0d336405524576c55c541
Joey Peterson <joey.peterson15@gmail.com>
2022-02-18 18:36:00 -0800

    asdfasdf

--- leetcode.py
+++ leetcode.py
@@ -914,3 +914,52 @@
 
 #   tracker[n] = helper(n - 1, tracker) + helper(n - 2, tracker) + helper(n - 3, tracker)
 #   return tracker[n]
+
+
+
+# 108. Convert Sorted Array to Binary Search Tree
+# Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+# A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+
+# class Solution:
+#     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
+#         def helper(left, right):
+
+#             if left > right:
+#                 return None
+
+#             mid = (left + right) // 2
+
+#             node = TreeNode(nums[mid])
+#             node.left = helper(left, mid - 1)
+#             node.right = helper(mid + 1, right)
+#             return node
+
+#         return helper(0, len(nums) - 1)
+
+# 109. Convert Sorted List to Binary Search Tree
+# Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+# For this problem, a height-balanced binary tree is defined as:
+# a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+# class Solution:
+#     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+#         vals = []
+#         curr = head
+#         while curr:
+#             vals.append(curr.val)
+#             curr = curr.next
+
+#         def helper(left, right):
+#             if left > right:
+#                 return None
+
+#             mid = (left + right) // 2
+#             node = TreeNode(vals[mid])
+#             node.left = helper(left, mid - 1)
+#             node.right = helper(mid + 1, right)
+#             return node
+
+#         return helper(0, len(vals) - 1)
