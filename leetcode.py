2c630f67710116c173a31c54c293e6222957f85f
Joey Peterson <joey.peterson15@gmail.com>
2022-02-21 09:16:16 -0800

    asdfasdf

--- leetcode.py
+++ leetcode.py
@@ -1156,3 +1156,37 @@
 #                 if levels[i][j] == u and (j + 1) in range(len(levels[i])):
 #                     return levels[i][j + 1]
 #         return None
+
+
+
+
+
+
+
+
+# 235. Lowest Common Ancestor of a Binary Search Tree
+# Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between
+# two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+# class Solution:
+#     def lowestCommonAncestor(self, root, p, q):
+#         """
+#         :type root: TreeNode
+#         :type p: TreeNode
+#         :type q: TreeNode
+#         :rtype: TreeNode
+#         """
+#         # Value of current node or parent node.
+#         parent_val = root.val
+
+
+
+#         # If both p and q are greater than parent
+#         if p.val > parent_val and q.val > parent_val:
+#             return self.lowestCommonAncestor(root.right, p, q)
+#         # If both p and q are lesser than parent
+#         elif p.val < parent_val and q.val < parent_val:
+#             return self.lowestCommonAncestor(root.left, p, q)
+#         # We have found the split point, i.e. the LCA node.
+#         else:
+#             return root
