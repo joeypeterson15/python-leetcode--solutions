4dc4c3bc2adb6d12a10e4022b0c7d20bdd62358e
Joey Peterson <joey.peterson15@gmail.com>
2022-03-22 15:26:49 -0700

    adsfasdf

--- leetcode.py
+++ leetcode.py
@@ -1996,20 +1996,20 @@
 
 
 
-56. Merge Intervals
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+# 56. Merge Intervals
+# class Solution:
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
-        intervals.sort(key=lambda x: x[0])
-        res = []
+#         intervals.sort(key=lambda x: x[0])
+#         res = []
 
-        for interval in intervals:
-            if not res or res[-1][1] < interval[0]:
-                res.append(interval)
-            else:
-                res[-1][1] = max(interval[1], res[-1][1])
+#         for interval in intervals:
+#             if not res or res[-1][1] < interval[0]:
+#                 res.append(interval)
+#             else:
+#                 res[-1][1] = max(interval[1], res[-1][1])
 
-        return res
+#         return res
 
 # Meeting Rooms
 # class Solution:
