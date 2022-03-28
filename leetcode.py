582a703b34841dfa20dbb349345dfedf1848fd51
Joey Peterson <joey.peterson15@gmail.com>
2022-03-28 09:19:08 -0700

    asdf

--- leetcode.py
+++ leetcode.py
@@ -2483,3 +2483,20 @@
 #         """
 #         node.val=node.next.val
 #         node.next=node.next.next
+
+
+
+
+# 57. Insert Interval
+# class Solution:
+#     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+#         res = []
+#         intervals.append(newInterval)
+#         intervals.sort(key=lambda i:i[0])
+#         res.append(intervals[0])
+#         for i in range(1, len(intervals)):
+#             if intervals[i][0] <= res[-1][1]:
+#                 res[-1][1] = max(res[-1][1], intervals[i][1])
+#             else:
+#                 res.append(intervals[i])
+#         return res
