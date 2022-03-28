f513e4773d298d14449560ee77fa22dcdd2d6998
Joey Peterson <joey.peterson15@gmail.com>
2022-03-27 18:57:54 -0700

    asdfasdf

--- leetcode.py
+++ leetcode.py
@@ -2469,3 +2469,17 @@
 #             curr = curr.next
 
 #         return sentinal.next
+
+
+
+
+
+# 237. Delete Node in a Linked List
+# class Solution:
+#     def deleteNode(self, node):
+#         """
+#         :type node: ListNode
+#         :rtype: void Do not return anything, modify node in-place instead.
+#         """
+#         node.val=node.next.val
+#         node.next=node.next.next
