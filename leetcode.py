4eebc3c215bf1e9167f645c0b91dd706586f5554
Joey Peterson <joey.peterson15@gmail.com>
2022-04-12 11:19:58 -0700

    adfadsf

--- leetcode.py
+++ leetcode.py
@@ -2825,3 +2825,24 @@
 # from customers
 # left join orders on (orders.customerId = customers.id)
 # where orders.customerId is null
+
+
+
+
+# 125. Valid Palindrome
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         left = 0
+#         right = len(s) - 1
+
+#         while left < right:
+#             while left < right and not s[left].isalnum():
+#                 left += 1
+#             while left < right and not s[right].isalnum():
+#                 right -= 1
+#             if s[left].lower() != s[right].lower():
+#                 return False
+#             left += 1
+#             right -= 1
+
+#         return True
