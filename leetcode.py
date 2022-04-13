0990044e238873c03a064ad876abe4ee989f5c32
Joey Peterson <joey.peterson15@gmail.com>
2022-04-12 21:28:36 -0700

    asdfasdf

--- leetcode.py
+++ leetcode.py
@@ -2891,3 +2891,19 @@
 
 
 #         return -1
+
+
+# 451. Sort Characters By Frequency
+# class Solution:
+#     def frequencySort(self, s: str) -> str:
+
+#     # Count up the occurances.
+#         counts = collections.Counter(s)
+
+#         # Build up the string builder.
+#         string_builder = []
+#         for letter, freq in counts.most_common():
+#             # letter * freq makes freq copies of letter.
+#             # e.g. "a" * 4 -> "aaaa"
+#             string_builder.append(letter * freq)
+#         return "".join(string_builder)
