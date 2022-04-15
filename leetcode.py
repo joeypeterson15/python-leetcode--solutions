a884a342296285c97ea733679fa8b4f8958dbe01
Joey Peterson <joey.peterson15@gmail.com>
2022-04-15 15:34:25 -0700

    asdf

--- leetcode.py
+++ leetcode.py
@@ -3069,3 +3069,30 @@
 #                 right = L - 1
 
 #         return left - 1
+
+
+
+
+# 21. Merge Two Sorted Lists
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         curr1 = list1
+#         curr2 = list2
+
+#         if curr1 is None:
+#             return curr2
+#         elif curr2 is None:
+#             return curr1
+
+#         elif curr2.val >= curr1.val:
+#             curr1.next = self.mergeTwoLists(curr1.next, curr2)
+#             return curr1
+
+#         elif curr2.val < curr1.val:
+#             curr2.next = self.mergeTwoLists(curr1, curr2.next)
+#             return curr2
