8c2e51f5476d9fa8d7ccd8227070caccf6e1b910
Joey Peterson <joey.peterson15@gmail.com>
2022-04-16 16:00:51 -0700

    adsfadsf

--- leetcode.py
+++ leetcode.py
@@ -3131,3 +3131,19 @@
 #         changeNodeVals(root, newVals)
 
 #         return root
+
+
+
+
+
+# 532. K-diff Pairs in an Array
+# class Solution:
+#     def findPairs(self, nums: List[int], k: int) -> int:
+#         counter = collections.Counter(nums)
+#         count = 0
+#         for num in counter:
+#             if k > 0 and (k + num) in counter:
+#                 count += 1
+#             elif k == 0 and counter[num] > 1:
+#                 count += 1
+#         return count
